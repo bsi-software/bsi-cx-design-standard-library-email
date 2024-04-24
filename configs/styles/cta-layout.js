cadd0237e1ba8c7e53e3e6eb45aee7e396fe5674
Benjamin Vier <Benjamin.Vier@bsi-software.com>
2024-04-24 16:02:17 +0200

    Some Code Style

--- configs/styles/cta-layout.js
+++ configs/styles/cta-layout.js
@@ -1,15 +1,16 @@
-const {cx, Version, bsiProperty} = require('@bsi-cx/design-build');
+const { cx, Version, bsiProperty } = require('@bsi-cx/design-build');
 
 const backgroundColor = bsiProperty('buttonBackgroundColor', '#2F4E66');
 const borderColor = bsiProperty('buttonBorderColor', '#2F4E66');
 const borderSize = bsiProperty('buttonBorderSize', 2).value;
 const horizontalPadding = bsiProperty('buttonHorizontalPadding', 30).value;
-const verticalPadding = bsiProperty ('buttonVerticalPadding', 10).value;
-const borderFilled = (backgroundColor.hex == borderColor.hex) ? 'border: ' + '0' +';' : '';
+const verticalPadding = bsiProperty('buttonVerticalPadding', 10).value;
+const borderAndBackgroundSame = backgroundColor.hex === borderColor.hex;
+const borderFilled = borderAndBackgroundSame ? 'border: 0;' : '';
 //if border color and background color are different a border is already set and not necessary
-const borderOutline = (backgroundColor.hex != borderColor.hex) ? '' : 'border: ' + borderSize + 'px ' + 'solid ' + borderColor +';';
-const paddingFilled = (backgroundColor.hex == borderColor.hex) ? 'padding:' + (verticalPadding+borderSize) + 'px ' + (horizontalPadding+borderSize) + 'px' +';' : '';
-const paddingOutline = (backgroundColor.hex == borderColor.hex) ? 'padding:' + verticalPadding + 'px ' + horizontalPadding + 'px' +';' : '';
+const borderOutline = borderAndBackgroundSame ? `border: ${borderSize}px solid ${borderColor};` : '';
+const paddingFilled = borderAndBackgroundSame ? `padding: ${(verticalPadding + borderSize)}px ${(horizontalPadding + borderSize)}px;` : '';
+const paddingOutline = borderAndBackgroundSame ? `padding: ${verticalPadding}px ${horizontalPadding}px;` : '';
 
 module.exports = cx.style
     .withIdentifier('cta-layout-b2fd9420')
@@ -25,29 +26,25 @@ module.exports = cx.style
                 cx.domManipulation
                     .withSelector('.bsi-cta-wrapper td')
                     .withAttribute('style')
-                    .withValue( 
-                        'background-color: ' + backgroundColor +';'+
-                        borderFilled),
+                    .withValue(`background-color: ${backgroundColor}; ${borderFilled}`),
                 cx.domManipulation
                     .withSelector('.bsi-cta')
                     .withAttribute('style')
                     .withValue(
-                        'color: ' + '#ffffff' +';' +
+                        'color: #ffffff;' +
                         paddingFilled)),
         cx.styleOption
-            .withStyleId('bsi-btn-outline')    
+            .withStyleId('bsi-btn-outline')
             /*.withLabel('colored, framed')*/
             .withLabel('Farbig umrandet')
             .withDomManipulations(
                 cx.domManipulation
                     .withSelector('.bsi-cta-wrapper td')
                     .withAttribute('style')
-                    .withValue( 
-                        'background-color: ' + 'transparent' +';'+ 
+                    .withValue(
+                        'background-color: transparent;' +
                         borderOutline),
                 cx.domManipulation
                     .withSelector('.bsi-cta')
                     .withAttribute('style')
-                    .withValue(
-                        'color: ' + backgroundColor +';' + 
-                        paddingOutline)));
+                    .withValue(`color: ${backgroundColor}; ${paddingOutline}`)));
